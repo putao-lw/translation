1b4429345a5b7074e4b9efbc4a9a4dbdaf6d67f3
putao-lw <linwei.work.1@gmail.com>
2026-08-05 20:28:46 +0800

    Fix Android JVM target compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,14 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 
 android { namespace = "com.putaolw.translation"; compileSdk = 35
     defaultConfig { applicationId = "com.putaolw.translation"; minSdk = 29; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
